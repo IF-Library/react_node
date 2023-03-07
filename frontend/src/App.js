2cbcc9a0a7f893546f8cda2a68da0a6c183cc996
Luis Gustavo Furtuozo Falci <78694561+LuisFalci@users.noreply.github.com>
2023-03-07 08:49:42 -0300

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,10 @@
-import './App.css';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Task from './pages/Task';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+   <>
+   <Navbar/>
+   </>
   );
 }
 
