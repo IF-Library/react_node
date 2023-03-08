b3e327082a245f9d26b1fa06dabf375351a5cd4a
Luis Gustavo Furtuozo Falci <78694561+LuisFalci@users.noreply.github.com>
2023-03-08 19:31:01 -0300

    Update taskRouter.js

--- backend/routes/taskRouter.js
+++ backend/routes/taskRouter.js
@@ -1,13 +1,12 @@
 const router = require("express").Router();
-const taskController = require("../controllers/taskController");
-const authMiddleware = require("../middlewares/auth");
+const { createTask, deleteTask, getAllTasks, getTask, updateTask } = require("../controllers/taskController");
+const auth = require("../middlewares/auth");
 
 // o authMiddleware entra para validar o token do usuário, só depois de validado prossegue para as rotas
-router.use(authMiddleware);
-router.route("/createTask").post((req, res) => taskController.createTask(req, res));
-router.route("/getTask").get((req, res) => taskController.getAllTasks(req, res));
-router.route("/getTask/:id").get((req, res) => taskController.getTask(req, res));
-router.route("/deleteTask/:id").delete((req, res) => taskController.deleteTask(req, res));
-router.route("/updateTask/:id").put((req, res) => taskController.updateTask(req, res));
+router.post("/createTask", auth, createTask);
+router.get("/getTasks", auth, getAllTasks);
+router.get("/getTask", auth, getTask);
+router.delete("/deleteTask/:id", auth, deleteTask);
+router.put("/updateTask/:id", auth, updateTask);
 
 module.exports = router;
