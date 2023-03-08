f2d2eca2cc8d038b28ee8ef34dff8f615f40544b
Luis Gustavo Furtuozo Falci <78694561+LuisFalci@users.noreply.github.com>
2023-03-08 19:30:57 -0300

    Delete authRouter.js

--- backend/routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const authController = require("../controllers/authController");
-
-router.route("/register").post((req, res) => authController.register(req, res));
-router.route("/authenticate").post((req, res) => authController.authenticate(req, res));
-router.route("/forgot_password").post((req, res) => authController.forgot_password(req, res));
-router.route("/reset_password").post((req, res) => authController.reset_password(req, res));
-
-module.exports = router;
